6447b2ee0a05ea9d028397ca17b35a2f756ae721
Taco H. in den Bosch <taco@heddesit.nl>
2025-10-21 17:13:36 +0200

    Initial commit

---
+++ PushoverClient.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Heddes.Rest;
+
+namespace Heddes.Pushover;
+
+public class PushoverService
+{
+	public const string SendMessageUrl = "https://api.pushover.net/1/messages.json";
+
+	public PushoverService(string apiKey, IHttpClient httpClient)
+	{
+		_apiKey = apiKey;
+		_httpClient = httpClient;
+
+		_jsonSerializationOptions = new JsonSerializerOptions
+			{ DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+	}
+
+	public virtual async Task SendMessage(PushoverMessage message)
+	{
+		if (string.IsNullOrEmpty(message.ApiToken))
+			message.ApiToken = _apiKey;
+
+		var json = JsonSerializer.Serialize(message, _jsonSerializationOptions);
+
+		var request = new HttpRequestMessage
+		{
+			Method = HttpMethod.Post,
+			RequestUri = new Uri(SendMessageUrl)
+		};
+		request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+		var response = await _httpClient.SendAsync(request);
+		var responseText = await response.Content.ReadAsStringAsync();
+
+		switch (response.StatusCode)
+		{
+			case HttpStatusCode.OK:
+				JsonSerializer.Deserialize<SendMessageResponse>(responseText, _jsonSerializationOptions);
+				break;
+
+			default:
+				throw new InvalidOperationException($"Unexpected status code {response.StatusCode} from Pushover.");
+		}
+	}
+
+	#region Private fields
+
+	private readonly string _apiKey;
+	private readonly IHttpClient _httpClient;
+	private readonly JsonSerializerOptions _jsonSerializationOptions;
+
+	#endregion
+}
---
+++ PushoverMessage.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Heddes.Pushover;
+
+public class PushoverMessage
+{
+	[JsonPropertyName("token")] public string ApiToken { get; set; }
+
+	[JsonPropertyName("attachment_type")] public string AttachmentType { get; set; }
+
+	[JsonPropertyName("device")] public string Device { get; set; }
+
+	[JsonPropertyName("expire")] public int ExpireInSeconds { get; set; }
+
+	[JsonPropertyName("html")] public int Html { get; set; }
+
+	[JsonPropertyName("message")] public string Message { get; set; }
+
+	[JsonPropertyName("priority")] public int Priority { get; set; }
+
+	[JsonPropertyName("retry")] public int RetryAfterSeconds { get; set; }
+
+	[JsonPropertyName("sound")] public string Sound { get; set; }
+
+	[JsonPropertyName("timestamp")] public long Timestamp { get; set; }
+
+	[JsonPropertyName("ttl")] public int? TimeToLive { get; set; }
+
+	[JsonPropertyName("title")] public string Title { get; set; }
+
+	[JsonPropertyName("url")] public string? Url { get; set; }
+
+	[JsonPropertyName("url_title")] public string UrlTitle { get; set; }
+
+	[JsonPropertyName("user")] public string User { get; set; }
+}
+
+public class SendMessageResponse
+{
+	public SendMessageResponse()
+	{
+		RequestId = string.Empty;
+	}
+
+	[JsonPropertyName("errors")] public string[]? Errors { get; set; }
+
+	[JsonPropertyName("request")] public string RequestId { get; set; }
+
+	[JsonPropertyName("status")] public int Status { get; set; }
+}
